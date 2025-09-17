173c86dce32b1601e683f0d135ec8d659344de78
maxsh <max.shushanikov@gmail.com>
2025-09-17 23:15:18 +0200

    Initialize project

---
+++ src/main/java/org/planicore/PlanicoreApplication.java
@@ -0,0 +1,13 @@
+package org.planicore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanicoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanicoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/planicore/PlanicoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.planicore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanicoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
