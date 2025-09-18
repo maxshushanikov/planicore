657d90dff66de9b37923f8c1e9377344b1c511c2
maxsh <max.shushanikov@gmail.com>
2025-09-18 18:13:39 +0200

    Appends file application-test.properties

--- src/test/java/org/planicore/PlanicoreApplicationTests.java
+++ src/test/java/org/planicore/PlanicoreApplicationTests.java
@@ -2,12 +2,14 @@ package org.planicore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PlanicoreApplicationTests {
 
     @Test
     void contextLoads() {
+        // Тест проверяет, что контекст Spring загружается корректно
     }
-
 }
