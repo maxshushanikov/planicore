3f4e3ad5d9f4237e5ae5c9f32ef095501697b349
maxsh <max.shushanikov@gmail.com>
2025-09-18 23:04:11 +0200

    Appends dependencies for mapping

--- src/main/java/org/planicore/server/repository/TaskRepository.java
+++ src/main/java/org/planicore/server/repository/TaskRepository.java
@@ -1,6 +1,6 @@
 package org.planicore.server.repository;
 
-import org.gradle.api.Task;
+import org.planicore.server.domain.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
